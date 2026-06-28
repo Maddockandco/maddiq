e52fe0acdcf1381a8db9fed235a4e2ce6eefc8a3
Maddockandco <info@maddockandco.com>
2026-06-28 11:20:25 +0100

    Create layout.tsx

---
+++ app/clients/layout.tsx
@@ -0,0 +1,18 @@
+import Sidebar from '@/components/layout/Sidebar'
+
+export default function ClientsLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="flex min-h-screen bg-brand-light">
+      <Sidebar />
+      <div className="flex-1 flex flex-col ml-64">
+        <main className="flex-1 p-8">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}
