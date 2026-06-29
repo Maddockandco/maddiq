79c5d796f52ebe5b31e4fb044dc146e5cb9a300d
Maddockandco <info@maddockandco.com>
2026-06-29 09:06:46 +0100

    Update layout.tsx

--- app/clients/[id]/layout.tsx
+++ app/clients/[id]/layout.tsx
@@ -8,12 +8,12 @@ export default function ClientDetailLayout({
 }) {
   return (
     <div className="flex min-h-screen bg-brand-light">
-      <div className="hidden lg:flex">
+      <div className="hidden lg:block">
         <Sidebar />
       </div>
       <MobileHeader />
-      <div className="flex-1 min-w-0 flex flex-col lg:ml-64">
-        <main className="flex-1 p-4 lg:p-8 pt-20 lg:pt-8 min-w-0 overflow-x-hidden">
+      <div className="flex-1 min-w-0 lg:ml-64">
+        <main className="p-4 lg:p-8 pt-20 lg:pt-8 min-w-0 overflow-x-hidden">
           {children}
         </main>
       </div>
