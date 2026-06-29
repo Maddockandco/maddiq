aeb589df68fa56b8a0b67fe514d8d648e4494727
Maddockandco <info@maddockandco.com>
2026-06-29 08:02:27 +0100

    Update layout.tsx

--- app/pipeline/layout.tsx
+++ app/pipeline/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from '@/components/layout/Sidebar'
+import MobileHeader from '@/components/layout/MobileHeader'
 
 export default function PipelineLayout({
   children,
@@ -7,9 +8,12 @@ export default function PipelineLayout({
 }) {
   return (
     <div className="flex min-h-screen bg-brand-light">
-      <Sidebar />
-      <div className="flex-1 flex flex-col ml-64">
-        <main className="flex-1 p-8">
+      <div className="hidden lg:block">
+        <Sidebar />
+      </div>
+      <MobileHeader />
+      <div className="flex-1 flex flex-col lg:ml-64">
+        <main className="flex-1 p-4 lg:p-8 pt-20 lg:pt-8">
           {children}
         </main>
       </div>
