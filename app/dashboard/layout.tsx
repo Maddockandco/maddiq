f2b1af47b617e1ac926ec32ebb782e7d7293823a
Maddockandco <info@maddockandco.com>
2026-06-29 07:58:00 +0100

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from '@/components/layout/Sidebar'
+import MobileHeader from '@/components/layout/MobileHeader'
 
 export default function DashboardLayout({
   children,
@@ -7,9 +8,15 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="flex min-h-screen bg-brand-light">
-      <Sidebar />
-      <div className="flex-1 flex flex-col ml-64">
-        <main className="flex-1 p-8">
+      {/* Desktop sidebar */}
+      <div className="hidden lg:block">
+        <Sidebar />
+      </div>
+      {/* Mobile header */}
+      <MobileHeader />
+      {/* Main content */}
+      <div className="flex-1 flex flex-col lg:ml-64">
+        <main className="flex-1 p-4 lg:p-8 pt-20 lg:pt-8">
           {children}
         </main>
       </div>
