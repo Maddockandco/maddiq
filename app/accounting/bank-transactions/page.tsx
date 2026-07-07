f12b069ca3949a99c51440a56b78a5178d7a86ee
Maddockandco <info@maddockandco.com>
2026-07-07 15:32:19 +0100

    Delete app/accounting/bank-transactions/page.tsx

--- app/accounting/bank-transactions/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import BankTransactions from '@/components/accounting/BankTransactions'
-
-export default function BankTransactionsPage({ params }: { params: { clientId: string } }) {
-  return <BankTransactions clientId={params.clientId} />
-}
