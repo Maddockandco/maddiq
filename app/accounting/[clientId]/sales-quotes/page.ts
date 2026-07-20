da4c55683b03ea21c921f8ae14b8f4e7699892ef
Maddockandco <info@maddockandco.com>
2026-07-20 12:58:07 +0100

    Delete app/accounting/[clientId]/sales-quotes/page.ts

--- app/accounting/[clientId]/sales-quotes/page.ts
+++
@@ -1,6 +0,0 @@
-'use client'
-import SalesQuotes from '@/components/accounting/SalesQuotes'
-
-export default function SalesQuotesPage({ params }: { params: { clientId: string } }) {
-  return <SalesQuotes clientId={params.clientId} />
-}
